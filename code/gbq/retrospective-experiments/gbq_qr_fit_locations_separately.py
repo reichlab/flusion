34c08700998029d6b1e0c8f2f8886c80aac745e0
Evan Ray <elray1@users.noreply.github.com>
2024-05-20 09:55:26 -0400

    Update code/gbq/retrospective-experiments/gbq_qr_fit_locations_separately.py
    Co-authored-by: Becky Sweger <bsweger@gmail.com>

--- code/gbq/retrospective-experiments/gbq_qr_fit_locations_separately.py
+++ code/gbq/retrospective-experiments/gbq_qr_fit_locations_separately.py
@@ -27,6 +27,10 @@ output_root = '../../retrospective-hub/model-output'
 commands = [f'python gbq.py --ref_date {ref_date} --output_root {output_root} --model_name gbq_qr_fit_locations_separately' \
                 for ref_date in missing_ref_dates]
 
-with Pool(processes=2) as pool:
+def main():
+    with Pool(processes=2) as pool:
+        pool.map(run_command, commands)
+
+if __name__ == '__main__':
     freeze_support()
-    pool.map(run_command, commands)
+    main()
